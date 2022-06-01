fc31ae563446f6a0b01f2d73389f6d3b24901c28
kerokerotakero <105433314+kerokerotakero@users.noreply.github.com>
2022-06-02 08:17:59 +0900

    Add files via upload

---
+++ herokutest/main.py
@@ -0,0 +1,9 @@
+import datetime as dt
+import pandas as pd
+
+def test():
+    print('hello world!!')
+    print(dt.datetime.utcnow().strftime('%Y/%m/%d %H:%M'))
+    print(pd.DataFrame(columns=['A', 'B', 'C'], index=[i for i in range(2)]))
+
+test()
